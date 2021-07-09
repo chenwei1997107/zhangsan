658b41384711181dfd681770b2415784f9b7a9c7
chenwei <1353647867@qq.com>
2021-07-09 21:18:30 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,58 @@
+# print(123)
+# print(12.34)
+# print(True)
+# print(())
+# print([])
+# print({})
+# print("你好")
+# print("哈哈",147)
+# print("打你"*10)
+# print(50*"我爱你")
+# print(100/2)
+
+
+# print(123-23)
+# print(5<8)
+"""
+# c = "哈哈"
+# print(c)
+# """
+
+# c = type("klsldf")
+# print(c)
+
+# print(type(123))
+# print(type(1.32))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+# print(type("好的"))
+
+# c = float("123.5")
+# print(type(c))
+
+# a = int(input("请输入:"))
+# c = int(input("请输入:"))
+# print("input获取的内容：",a+c)
+
+# a = "klwjfklajwfalkfj;wjf;jakfjwe;ljf;ajflkjadl;f;lajf;ekwflma;lfk;wjfk"
+# print(len(a))
+
+
+
+# a = (input("请写入:"))
+# b = (input("请写入:"))
+# print("=",len(a+b))
+
+# print("这是一个猜数字游戏")
+# a = input("请说")
+# b = input("请说")
+# print("长度:",len(a+b))
+# print("你真棒")
+
+
+a = input("请输入:")
+b = input("请输入:")
+print("两段的长度是多少",len(a+b))
+
